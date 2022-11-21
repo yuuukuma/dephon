512aa129f6cb257e00682fb395c5c4cafff089ee
yuuukuma <yuuukuma@gmail.com>
2022-11-21 13:29:34 +0900

    Refactor

--- dephon/cli/main.py
+++ dephon/cli/main.py
@@ -25,27 +25,27 @@ def parse_args_main(args):
     subparsers = parser.add_subparsers()
 
     # -- make_ccd_init_and_dirs -----------------------------------
-    parser_make_cpp_init = subparsers.add_parser(
+    parser_make_ccd_init = subparsers.add_parser(
         name="make_ccd_init_and_dirs",
         description="",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         aliases=['ci'])
 
-    parser_make_cpp_init.add_argument(
+    parser_make_ccd_init.add_argument(
         "-ed", "--excited_dir", type=Path, required=True,
         help="Directory for an excited excited state defect.")
-    parser_make_cpp_init.add_argument(
+    parser_make_ccd_init.add_argument(
         "-gd", "--ground_dir", type=Path, required=True,
         help="Directory for a ground state defect.")
-    parser_make_cpp_init.add_argument(
+    parser_make_ccd_init.add_argument(
         "-egr", "--e_to_g_div_ratios", type=float, nargs="+",
         default=[-0.4, -0.2, 0.2, 0.4, 0.6, 0.8],
         help="Dividing ratios from excited state to ground state structures.")
-    parser_make_cpp_init.add_argument(
+    parser_make_ccd_init.add_argument(
         "-ger", "--g_to_e_div_ratios", type=float, nargs="+",
         default=[-0.4, -0.2, 0.2, 0.4, 0.6, 0.8],
         help="Dividing ratios from ground state to excited state structures.")
-    parser_make_cpp_init.set_defaults(
+    parser_make_ccd_init.set_defaults(
         func=make_ccd_init_and_dirs)
     # ------------------------------------------------------------------------
     return parser.parse_args(args)
