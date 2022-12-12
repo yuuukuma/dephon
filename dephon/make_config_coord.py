aea69d39c6a8dd9afaf86a805e47bdaf0045a4a0
yuuukuma <yuuukuma@gmail.com>
2022-12-12 10:02:27 +0900

    Modify make_cc_init

--- dephon/make_config_coord.py
+++ dephon/make_config_coord.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2022 Kumagai group.
-
+from pydefect.analyzer.band_edge_states import PerfectBandEdgeState
 from pydefect.analyzer.calc_results import CalcResults
+from pydefect.analyzer.unitcell import Unitcell
 from pydefect.corrections.efnv_correction import ExtendedFnvCorrection
 
 from dephon.config_coord import CcdInit
@@ -11,7 +12,10 @@ def make_ccd_init(name: str,
                   excited_calc_results: CalcResults,
                   ground_calc_results: CalcResults,
                   excited_efnv_corr: ExtendedFnvCorrection,
-                  ground_efnv_corr: ExtendedFnvCorrection) -> CcdInit:
+                  ground_efnv_corr: ExtendedFnvCorrection,
+                  unitcell: Unitcell,
+                  p_band_edge_state: PerfectBandEdgeState,
+                  ) -> CcdInit:
 
     assert abs(excited_efnv_corr.charge - ground_efnv_corr.charge) == 1
     return CcdInit(
@@ -23,4 +27,8 @@ def make_ccd_init(name: str,
         excited_energy=excited_calc_results.energy,
         excited_energy_correction=excited_efnv_corr.correction_energy,
         ground_energy_correction=ground_efnv_corr.correction_energy,
-        ground_energy=ground_calc_results.energy)
+        ground_energy=ground_calc_results.energy,
+        vbm=unitcell.vbm,
+        cbm=unitcell.cbm,
+        supercell_vbm=p_band_edge_state.vbm_info.energy,
+        supercell_cbm=p_band_edge_state.cbm_info.energy)
--- dephon/make_config_coord_test.py
+++ dephon/make_config_coord_test.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2022 Kumagai group.
-
+from pydefect.analyzer.band_edge_states import PerfectBandEdgeState, EdgeInfo, \
+    OrbitalInfo
 from pydefect.analyzer.calc_results import CalcResults
+from pydefect.analyzer.unitcell import Unitcell
 from pydefect.corrections.efnv_correction import ExtendedFnvCorrection
 
 from dephon.config_coord import CcdInit
@@ -27,11 +29,23 @@ def test_make_cc_init(
     g_correction.correction_energy = 1000.0
     g_correction.charge = 0
 
+    unitcell = mocker.Mock(spec=Unitcell, autospec=True)
+    unitcell.vbm = 0.0
+    unitcell.cbm = 10.0
+
+    p_band_edge_state = mocker.Mock(spec=PerfectBandEdgeState, autospec=True)
+    p_band_edge_state.vbm_info = EdgeInfo(1, (0.0, 0.0, 0.0),
+                                          OrbitalInfo(2.0, {}, 0.0))
+    p_band_edge_state.cbm_info = EdgeInfo(1, (0.0, 0.0, 0.0),
+                                          OrbitalInfo(8.0, {}, 0.0))
+
     actual = make_ccd_init("Va_O1",
                            excited_calc_results,
                            ground_calc_results,
                            e_correction,
-                           g_correction)
+                           g_correction,
+                           unitcell,
+                           p_band_edge_state)
     expected = CcdInit("Va_O1",
                        excited_structure=excited_structure,
                        ground_structure=ground_structure,
@@ -40,5 +54,7 @@ def test_make_cc_init(
                        excited_energy=10.0,
                        excited_energy_correction=100.0,
                        ground_energy=1.0,
-                       ground_energy_correction=1000.0)
+                       ground_energy_correction=1000.0,
+                       vbm=0.0, cbm=10.0,
+                       supercell_vbm=2.0, supercell_cbm=8.0)
     assert actual == expected
