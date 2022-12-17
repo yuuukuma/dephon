54edb211cf05b2438c0bc3a8ef77f0d42d14d5f1
yuuukuma <yuuukuma@gmail.com>
2022-12-17 10:26:13 +0900

    Modify config_coord.py

--- dephon/config_coord.py
+++ dephon/config_coord.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2022 Kumagai group.
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List, Dict
 
+import matplotlib
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.axes import Axes
 from monty.json import MSONable
+from nonrad.ccd import get_omega_from_PES
 from pymatgen.analysis.defects.ccd import get_dQ
 from pymatgen.core import Structure
 from scipy import interpolate
 from tabulate import tabulate
+from vise.util.logger import get_logger
 from vise.util.matplotlib import float_to_int_formatter
 from vise.util.mix_in import ToJsonFileMixIn
+
+logger = get_logger(__name__)
 
 
 def get_dR(ground: Structure, excited: Structure) -> float:
@@ -31,24 +37,66 @@ def get_dR(ground: Structure, excited: Structure) -> float:
 
 
 @dataclass
+class MinimumPointInfo(MSONable):
+    charge: int
+    structure: Structure
+    energy: float
+    energy_correction: float
+    site_symmetry: str
+    site_symmetry_opt_num: int
+
+    @property
+    def corrected_energy(self):
+        return self.energy + self.energy_correction
+
+
+@dataclass
 class CcdInit(MSONable, ToJsonFileMixIn):
     name: str
-    excited_structure: Structure  # excited excited structure
-    ground_structure: Structure  # final ground structure
-    excited_charge: int
-    ground_charge: int
-    excited_energy: float
-    excited_energy_correction: float
-    ground_energy: float
-    ground_energy_correction: float
+    ground_state: MinimumPointInfo
+    excited_state: MinimumPointInfo
     vbm: float
     cbm: float
     supercell_vbm: float
     supercell_cbm: float
+    multiplicity_by_symmetry_reduction: int = None
+
+    def __post_init__(self):
+        if abs(self.ground_state.charge - self.excited_state.charge) != 1:
+            logger.warning("The charge difference is not 1. You must know what"
+                           "you are doing.")
+
+    @property
+    def ground_structure(self):
+        return self.ground_state.structure
+
+    @property
+    def excited_structure(self):
+        return self.excited_state.structure
+
+    @property
+    def ground_charge(self):
+        return self.ground_state.charge
+
+    @property
+    def excited_charge(self):
+        return self.excited_state.charge
+
+    @property
+    def ground_corrected_energy(self):
+        return self.ground_state.corrected_energy
+
+    @property
+    def excited_corrected_energy(self):
+        return self.excited_state.corrected_energy
 
     @property
     def dQ(self):
         return get_dQ(self.excited_structure, self.ground_structure)
+
+    @property
+    def zero_phonon_line(self):
+        return self.excited_corrected_energy - self.ground_corrected_energy
 
     @property
     def dR(self):
@@ -58,35 +106,34 @@ class CcdInit(MSONable, ToJsonFileMixIn):
     def modal_mass(self):
         return (self.dQ / self.dR) ** 2
 
+    @property
+    def charge_change(self):
+        return self.excited_charge - self.ground_charge
+
     def __str__(self):
-        charge_diff = self.ground_charge - self.excited_charge
-        if charge_diff == -1:
-            trapped_carrier = "e-"
-        elif charge_diff == 1:
-            trapped_carrier = "h+"
-        else:
-            raise ValueError("The charge difference between excited and ground "
-                             "states is neither 1 nor -1")
+        result = [f"name: {self.name}"]
+        table = [["vbm", self.vbm, "supercell vbm", self.supercell_vbm],
+                 ["cbm", self.cbm, "supercell cbm", self.supercell_cbm],
+                 ["dQ (amu^0.5 Å)", self.dQ],
+                 ["dR (Å)", self.dR],
+                 ["M (amu)", self.modal_mass]]
+        result.append(tabulate(table, tablefmt="plain", floatfmt=".3f"))
 
-        result = [["name", self.name],
-                  ["vbm", round(self.vbm, 2),
-                   "supercell vbm", round(self.supercell_vbm, 2)],
-                  ["cbm", round(self.cbm, 2),
-                   "supercell cbm", round(self.supercell_cbm, 2)],
-                  ["dQ (amu^0.5 Å)", round(self.dQ, 2)],
-                  ["dR (Å)", round(self.dR, 2)],
-                  ["M (amu)", round(self.modal_mass, 2)]]
-        excited_state = f"{self.name}_{self.excited_charge} + {trapped_carrier}"
-        result.append(["Excited state:", excited_state,
-                       "energy:", self.excited_energy,
-                       "correction:", round(self.excited_energy_correction, 3)])
+        result.append("-" * 60)
 
-        ground_state = f"{self.name}_{self.ground_charge}"
-        result.append(["Ground state:", ground_state,
-                       "energy:", self.ground_energy,
-                       "correction:", round(self.ground_energy_correction, 3)])
+        headers = ["state", "site symm", "energy", "correction",
+                   "corrected energy"]
+        table = []
+        for s in [self.excited_state, self.ground_state]:
+            state_str = f"{self.name}_{s.charge}"
+            table.append([state_str, s.site_symmetry, s.energy,
+                          s.energy_correction, s.corrected_energy])
 
-        return tabulate(result, tablefmt="plain")
+        result.append(
+            tabulate(table, tablefmt="plain", headers=headers, floatfmt=".3f",
+                     stralign="center"))
+        result.append(f"ZPL: {self.zero_phonon_line:.3f}")
+        return "\n".join(result)
 
 
 @dataclass
@@ -105,17 +152,79 @@ class ImageStructureInfo(MSONable, ToJsonFileMixIn):
 class Ccd(MSONable, ToJsonFileMixIn):
     image_infos: Dict[str, List[ImageStructureInfo]]
     name: str = None
+    fitting_q_ranges: Dict[str, List[float]] = field(default_factory=dict)
 
     def __post_init__(self):
         for v in self.image_infos.values():
             v.sort(key=lambda x: x.dQ)
 
     @property
-    def min_energy(self):
+    def lowest_energy(self):
         energies = []
         for imag_infos in self.image_infos.values():
             energies += [imag_info.corrected_energy for imag_info in imag_infos]
         return min(energies)
+
+    def set_q_range(self, image_name, min_q, max_q):
+        if min_q is None and max_q is None:
+            logger.info(f"Quadratic fitting range for {image_name} is unset.")
+            self.fitting_q_ranges.pop(image_name)
+            return
+        if min_q is None or max_q is None:
+            logger.warning(f"To set quadratic fitting range, both min and max"
+                           f"values need to be set.")
+            return
+
+        logger.info(f"Quadratic fitting for {image_name} is set to "
+                    f"[{min_q}, {max_q}].")
+        self.fitting_q_ranges[image_name] = [min_q, max_q]
+
+    def q_range(self, image_name):
+        return self.fitting_q_ranges.get(image_name, None)
+
+    def omega(self,
+              image_name: str,
+              ax: Axes = None,
+              energy_std: float = 0.0):
+        dQs, energies = [], []
+        q_range = self.q_range(image_name)
+        if q_range is None:
+            raise ValueError("To calculate omega, set Q range.")
+
+        for v in self.image_infos[image_name]:
+            if q_range[0] < v.dQ < q_range[1]:
+                dQs.append(v.dQ)
+                energies.append(v.corrected_energy - energy_std)
+
+        if len(dQs) < 2:
+            raise ValueError("The number of Q points is not sufficient.")
+
+        return get_omega_from_PES(np.array(dQs), np.array(energies), ax=ax)
+
+    def __str__(self):
+        result = [f"name: {self.name}"]
+        headers = ["dQ", "energy", "corr", "corr energy (rel)", "corr method"]
+        for image_name, imag_structures in self.image_infos.items():
+            result.append(image_name)
+            table = []
+            for imag_structure in imag_structures:
+                table.append([imag_structure.dQ,
+                              imag_structure.energy,
+                              imag_structure.correction,
+                              imag_structure.corrected_energy - self.lowest_energy,
+                              imag_structure.correction_type])
+            tabu = tabulate(table, tablefmt="plain", floatfmt=".2f",
+                            headers=headers)
+            result.append(tabu)
+
+            q_range = self.q_range(image_name)
+            if q_range:
+                q_range = f"[{q_range[0]}, {q_range[1]}]"
+                result.append(f"Fitting Q range {q_range}")
+                result.append(f"omega: {self.omega(image_name):.3f}")
+            result.append("")
+
+        return "\n".join(result)
 
 
 def spline3(x, y, point, deg):
@@ -144,13 +253,17 @@ class CcdPlotter:
         self.plt.tight_layout()
 
     def _add_ccd(self):
-        min_e = self._ccd.min_energy
+        min_e = self._ccd.lowest_energy
         for more_name, v in self._ccd.image_infos.items():
             dQs = [vv.dQ for vv in v]
             energies = [vv.corrected_energy - min_e for vv in v]
             x, y = spline3(dQs, energies, 100, self._spline_deg)
             self.plt.plot(x, y, label=more_name)
             self.plt.scatter(dQs, energies, marker='o')
+
+            ax = self.plt.gca()
+            if self._ccd.fitting_q_ranges.get(more_name, None):
+                self._ccd.omega(more_name, ax=ax, energy_std=min_e)
 
     def _set_labels(self):
         ax = self.plt.gca()
--- dephon/config_coord_test.py
+++ dephon/config_coord_test.py
@@ -32,11 +32,18 @@ def test_ccd_dQ(ccd_init):
 
 def test_ccd_string(ccd_init):
     actual = ccd_init.__str__()
-    expected = """dQ              2.46
-dR              2.45
-M               1.01
-Excited state:  Va_O_0 + e-  energy:  -1  correction:  -1
-Ground state:   Va_O_-1      energy:  -2  correction:  -2"""
+    print(ccd_init)
+    expected = """name: Va_O
+vbm             1.012  supercell vbm  1.102
+cbm             2.012  supercell cbm  1.912
+dQ (amu^0.5 Å)  2.459
+dR (Å)          2.449
+M (amu)         1.008
+------------------------------------------------------------
+ state    site symm     energy    correction    corrected energy
+Va_O_0        2         -1.012        -1.123              -2.136
+Va_O_-1      2mm        -2.012        -2.123              -4.136
+ZPL: 2.000"""
     assert actual == expected
 
 
@@ -50,22 +57,32 @@ def ccd():
     return Ccd(name="Va_O1",
                image_infos={"q=0": [ImageStructureInfo(9.4, 1.0, 12.0),
                                     ImageStructureInfo(10.0, 1.0, 10.0),
-                                    ImageStructureInfo(10.4, 1.0, 8.0),
-                                    ImageStructureInfo(11.6, 1.0, 6.0),
-                                    ImageStructureInfo(13.6, 1.0, 4.0),
+                                    ImageStructureInfo(10.4, 1.0, 6.0),
+                                    ImageStructureInfo(10.8, 1.0, 4.0),
+                                    ImageStructureInfo(13.6, 1.0, 3.0),
                                     ImageStructureInfo(16.4, 1.0, 2.0),
                                     ImageStructureInfo(20.0, 1.0, 0.0)],
                             "q=1": [ImageStructureInfo(8.4, 1.0, -2.0),
-                                    ImageStructureInfo(9.0, 1.0, 0.0),
-                                    ImageStructureInfo(9.4, 1.0, 2.0),
-                                    ImageStructureInfo(10.6, 1.0, 4.0),
-                                    ImageStructureInfo(12.6, 1.0, 6.0),
-                                    ImageStructureInfo(15.4, 1.0, 8.0),
-                                    ImageStructureInfo(19.0, 1.0, 10.0)]})
+                                    ImageStructureInfo(9.0, 1.0, -3.5),
+                                    ImageStructureInfo(9.4, 1.0, -2.0),
+                                    ImageStructureInfo(10.6, 1.0, -1.0),
+                                    ImageStructureInfo(12.6, 1.0, 1.0),
+                                    ImageStructureInfo(15.4, 1.0, 4.0),
+                                    ImageStructureInfo(19.0, 1.0, 10.0)]},
+               fitting_q_ranges={"q=1": [10.0, 20.0]})
 
 
-def test_ccd_min_energy(ccd):
-    assert ccd.min_energy == -1.0
+def test_ccd_omega_(ccd):
+    ccd.set_q_range("q=0", 9.0, 21.0)
+    assert ccd.omega(image_name="q=0") == 0.033121755227466375
+
+
+def test_ccd_omega_str(ccd):
+    print(ccd)
+
+
+def test_ccd_lowest_energy(ccd):
+    assert ccd.lowest_energy == -2.5
 
 
 def test_plot_ccd(ccd):
